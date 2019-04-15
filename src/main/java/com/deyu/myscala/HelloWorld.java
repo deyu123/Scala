2e8591d2c2ee68eb67df7b6ed45d5839821bdece
zhangdeyu <1432360132@qq.com>
2019-04-15 22:51:43 +0800

    1.add commit scala

---
+++ src/main/java/com/deyu/myscala/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.deyu.myscala;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hell java");
+    }
+}
---
+++ src/main/scala/com/deyu/myscala/Helloworld.scala
@@ -0,0 +1,8 @@
+package com.deyu.myscala
+
+object Helloworld {
+  def main(args: Array[String]): Unit = {
+    println("hello, scala")
+  }
+
+}
